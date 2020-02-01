120005321d5aa39873549757f8dfbdca49f7e4b4
AlejandroSherman <alex@shermanart.net>
2020-02-01 13:16:29 -0800

    Added intial implementation of or.cpp

--- src/or.cpp
+++ src/or.cpp
@@ -1,1 +1,21 @@
-//stub of or connector file
+#ifndef __OR_CPP__
+#define __OR_CPP__
+
+#include "../header/base.h"
+#include "../header/connector.h"
+#inlcude <iostream>
+
+using namespace std;
+
+class Base;
+
+class Or : public Connector {
+public:
+  Or() { }
+  Or(Base *left, Base *right) : Connector(left, right) { }
+  bool execute() {
+    return (left->execute()) || (right->execute());
+  }
+};
+
+#endif
