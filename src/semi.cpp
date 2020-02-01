391cd7adaa87c7f88ec183222bf037f1e0409ebb
AlejandroSherman <alex@shermanart.net>
2020-02-01 13:19:29 -0800

    Added intitial implementation of semi.cpp

--- src/semi.cpp
+++ src/semi.cpp
@@ -1,1 +1,22 @@
-//stub of semi connector file
+#ifndef __SEMI_CPP__
+#define __SEMI_CPP__
+
+#include "../header/base.h"
+#include "../header/connector.h"
+#inlcude <iostream>
+
+using namespace std;
+
+class Base;
+
+class Semi : public Connector {
+public:
+  Semi() { }
+  Semi(Base *left, Base *right) : Connector(left, right) { }
+  bool execute() {
+     left->execute();
+     return right->execute();
+  }
+};
+
+#endif
