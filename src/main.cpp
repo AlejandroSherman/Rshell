fa04d07c035c0a460019c2e9ed4cf30712444485
AlejandroSherman <alex@shermanart.net>
2020-02-07 11:00:53 -0800

    Fixed issue where went out of bounds if only "cd" was entered.

--- src/main.cpp
+++ src/main.cpp
@@ -82,6 +82,9 @@ int main()
 	 cout << tokens.at(i) << endl;
  }
  //the current index of pos is the one after the previously found command
+ if (pos >= tokens.size()){ //fixes an issue where if only one command was passed in e.g : "cd" pos would be out of bounds
+	 pos = pos-1;
+ }
 
  command = update_command(tokens, valid_flags, command, pos);
  cout << endl << "Now the command is: " << command << endl;
@@ -92,7 +95,9 @@ int main()
 
 string argument;
 argument = set_argument(tokens, valid_anything, pos);
+
 argument.erase(0, 1); //argument always has a leading whitespace, removing it here
+
 cout << endl << "The command to be created is: " << command << " with argument: " << argument << endl;
 cout << endl << "Contents of tokens vector now are:" << endl;
 for (int i = 0; i < tokens.size(); ++i){
