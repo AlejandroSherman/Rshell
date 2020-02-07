4cbeef4b8bf67faa0ea25b563dd162a849ca7106
AlejandroSherman <alex@shermanart.net>
2020-02-06 19:28:29 -0800

    Added loops that get the valid command

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 string command_plus_flag(string cmd, string flag){
-	result = cmd + " " + flag;
+	string result = cmd + " " + flag;
 	return result;
 }
 
@@ -26,7 +26,17 @@ int main()
 
  vector <string> valid_commands {"ls", "mkdir", "echo", "git", "cd"}; //exit and comment are special and are handled in the parser
  vector <string> valid_flags{"-a", "-e", "-d", "-lR", "/"};
+ string command;
 
+ for (int i = 0; i < tokens.size(); ++i){
+	 for (int j = 0; j < valid_commands.size(); ++j){
+		 if (tokens.at(i) == valid_commands.at(j)){
+			 command = tokens.at(i);
+			 break;
+		 }
+	 }
+ }
+cout << command << endl;
 
 
 	return 0;
