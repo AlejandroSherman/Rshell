d89660cb4a2be1d8cf017b291778c9ca0ba051b1
AlejandroSherman <alex@shermanart.net>
2020-02-06 19:19:37 -0800

    Added command.cpp with a execute function that handles regular commands

---
+++ src/command.cpp
@@ -0,0 +1,14 @@
+#ifndef _COMMAND_CPP_
+#define _COMMAND_CPP_
+#include "../header/base.h"
+#include "../header/command.h"
+#include <iostream>
+#include <string>
+
+bool Command::execute(string argument){
+ //this funciton does a system call (and fork??) of the protected command
+ //the command passes in the string argument (Argument can be empty!)
+//if succeeds return true, if fails return false
+}
+
+#endif
