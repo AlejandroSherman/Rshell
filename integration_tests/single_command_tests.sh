b3103f9c18b1c4f23ead7857964608b8e977c783
AlejandroSherman <alex@shermanart.net>
2020-02-12 23:03:37 -0800

    gave permission to files and created stubs

--- integration_tests/single_command_tests.sh
+++ integration_tests/single_command_tests.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 echo "Executing ../rshell ls"
-../rshell "ls"
-echo "Executing ../rshell echo hello"
-../rshell "echo hello"
-echo "Executing ../rshell git status"
-../rshell "git status"
-echo "Executing ../rshell echo "hi" # && bye"
-../rshell "echo hi # && bye"
-echo "Executing ../rshell ls -a"
-../rshell "rm -f blah"
-echo "Executing ../rshell "
-../rshell "exit"
+../rshell < single_command_tests
