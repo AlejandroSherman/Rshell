b3f2cc104d56dc3398b7bc3f94c0e95abe2b7ac0
Ernie1065 <ernie6105@gmail.com>
2020-02-13 02:46:57 -0800

    Modified OR unit test

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -7,12 +7,12 @@
 
 //include test files from the unit test folder
 #include "and_test.h"
-//#include "base_test.h"
-//#include "command_test.h"
-//#include "connector_test.h"
-//#include "exit_test.h"
-//#include "or_test.h"
-//#include "semi_test.h"
+#include "base_test.h"
+#include "command_test.h"
+#include "connector_test.h"
+#include "exit_test.h"
+#include "or_test.h"
+#include "semi_test.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
