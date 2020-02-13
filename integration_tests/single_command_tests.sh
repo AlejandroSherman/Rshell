6a14260a2875165d7b8a9ecbfd46c4bb7ce6b1a3
AlejandroSherman <alex@shermanart.net>
2020-02-12 22:35:21 -0800

    Added initial implementation of single_command integration tests

--- integration_tests/single_command_tests.sh
+++ integration_tests/single_command_tests.sh
@@ -1,1 +1,14 @@
-#tests primarily for command executions 
+#!/bin/bash
+
+echo "Executing ../rshell ls"
+../rshell "ls"
+echo "Executing ../rshell echo hello"
+../rshell "echo hello"
+echo "Executing ../rshell git status"
+../rshell "git status"
+echo "Executing ../rshell echo "hi" # && bye"
+../rshell "echo hi # && bye"
+echo "Executing ../rshell ls -a"
+../rshell "rm -f blah"
+echo "Executing ../rshell "
+../rshell "exit"
