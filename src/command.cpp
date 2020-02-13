739a23532e32982a6ae7982a1372b6e322ff3657
Ernie1065 <ernie6105@gmail.com>
2020-02-12 21:12:59 -0800

    Fixed return values on command, return false when failed

--- src/command.cpp
+++ src/command.cpp
@@ -2,62 +2,62 @@
 #define _COMMAND_CPP_
 #include "../header/base.h"
 #include "../header/command.h"
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
 
+
 using namespace std;
 
 bool Command::execute() {
    //vector<string> x;
-    cout << "command object here!" << endl << "My command is: " << cmd << " My arguments are: ";
-    for(int i = 0; i < args.size(); ++i){
-      cout << args.at(i)<< " ";
-    }
-    cout << "The size of argument vector: " << args.size() << endl;
+    //cout << "command object here!" << endl << "My command is: " << cmd << " My arguments are: ";
+    //for(int i = 0; i < args.size(); ++i){
+    //    cout << args.at(i)<< " ";
+    //}
+    //cout << "The size of argument vector: " << args.size() << endl;
+    //cout << "Start executing ";
+    //cout << endl;
 
-
-    cout << "Start executing ";
+    const char* cmd_exe = cmd.c_str(); 
     bool result = true;
     char* arg[500];
-    cout << endl;
     int i = 0;
+    pid_t pid = fork();
     while (i < args.size())
     {
         arg[i] = (char*)args.at(i).c_str();
         i++;
     }
     arg[i] = NULL;
-
-    pid_t pid = fork();
+    
     if (pid < 0) //error on forking
         perror("fork() error");
 
     else if (pid == 0) {                    //child process
-        if (execvp(arg[0], arg) < 0) {      //error on execvp
-            printf("An execvp() error/n");
-            result = false;
-            exit(-1);
+        if (execvp(arg[0], arg) == -1) {      //error on execvp
+            perror("Execvp returned, error execvp");
+            exit(1);
         }
     }
 
     else if (pid > 0) {                     //parent process
         int status;
         if (waitpid(pid, &status, 0) < 0) {
-            perror("wait() error");
+            perror("Wait() error");
             result = false;
         }
-        if (WIFEXITED(status) == false)           //if normal, should return 0
+        int exit_status = WEXITSTATUS(status);
+        if (exit_status != 0)           //if normal, should return 0
         {
-            int exit_status = WEXITSTATUS(status);
-            printf("Exit status of the child was %d/n", exit_status);
+            printf("Child failed, exit statlsus of the child was %d\n", exit_status);
             result = false;
         }
     }
-    return result;
-
+    return result; 
 }
 
 #endif
