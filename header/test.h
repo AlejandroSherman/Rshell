5746b494b24c4eab934f0a4b510c6425e70c8169
AlejandroSherman <alex@shermanart.net>
2020-02-20 08:50:57 -0800

    Created initial header of test command

---
+++ header/test.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+#include "command.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class Test : public Command
+{
+private:
+	string wd;
+  vector<string> wds;
+public:
+	Test(string input, vector<string> arguments){
+    wd = input;
+    wds = arguments;
+	};
+	bool execute();
+};
+#endif
