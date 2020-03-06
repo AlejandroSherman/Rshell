7cf880e0c8b90ebe0571918c08b953a39f1c6c6d
AlejandroSherman <alex@shermanart.net>
2020-03-06 09:13:48 -0800

    Added input redirection files

---
+++ header/input.h
@@ -0,0 +1,17 @@
+#ifndef _INPUT_H_
+#define _INPUT_H_
+#include "connector.h"
+#include <string>
+
+using namespace std;
+
+class Input:public Connector{
+private:
+
+public:
+	Input(){ }
+	Input(Base *left, Base *right) : Connector(left, right) { }
+	bool execute();
+};
+
+#endif
---
+++ src/input.cpp
@@ -0,0 +1,27 @@
+#ifndef _INPUT_CPP_
+#define _INPUT_CPP_
+
+#include "../header/input.h"
+#include "../header/base.h"
+#include "../header/connector.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstring>
+#include <fcntl.h>
+
+using namespace std;
+
+class Base;
+class Connector;
+
+bool Input::execute() {
+  
+}
+
+
+
+#endif
